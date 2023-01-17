673451b382218cd43dc0c79ceb139780f0bb2347
Egor Kononenko <galfind@list.ru>
2023-01-18 00:11:45 +0300

    Made some privyau

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("2 Hello, World!");
+string username = Console.ReadLine();
+Console.WriteLine("Привяу, ");
+Console.WriteLine(username);
