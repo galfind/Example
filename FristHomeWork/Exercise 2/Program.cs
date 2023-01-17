c9a32ec6180717cf891f89b26a96cf37dc8a6437
Egor Kononenko <galfind@list.ru>
2023-01-18 00:37:06 +0300

    second exercise

---
+++ FristHomeWork/Exercise 2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите три числа");
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+int num3 = int.Parse(Console.ReadLine());
+if (num1 > num2 && num1 > num3 ){
+    Console.WriteLine("Max = " + num1);
+}
+else if (num2 > num1 && num2 > num3 ){
+    Console.WriteLine("Max = " + num2);
+}
+else{
+    Console.WriteLine("Max = " + num3);
+}
