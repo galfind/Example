7d90f551fc9c37ea52148f33a20d2e4f7f2fcaab
Egor Kononenko <galfind@list.ru>
2023-01-18 00:18:28 +0300

    First example

---
+++ FristHomeWork/Example 1.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите два числа");
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+if (num1 > num2){
+    Console.WriteLine("Max = " + num1);
+}
+else{
+    Console.WriteLine("Max = " + num2);
+}
