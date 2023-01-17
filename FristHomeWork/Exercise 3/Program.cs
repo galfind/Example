ca551a3a9bbffa85330847c2587eb356f21f07a7
Egor Kononenko <galfind@list.ru>
2023-01-18 00:53:08 +0300

    the end

---
+++ FristHomeWork/Exercise 3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число");
+int num1 = int.Parse(Console.ReadLine());
+if (num1 % 2 == 0){
+    Console.WriteLine("Четное ");
+}
+else{
+    Console.WriteLine("Нечетное");
+}
---
+++ FristHomeWork/Exercise 4/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число");
+int num1 = int.Parse(Console.ReadLine());
+for (int i = 0; i < num1; i++)
+{
+    if (i % 2 == 0){
+        Console.Write(i + " ");
+    }
+}
