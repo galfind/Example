ac35fda0dd4cd233460613c58650cce15c75ef12
Egor Kononenko <galfind@list.ru>
2023-02-07 01:09:43 +0300

    SOme changes

--- SixsthHomeWork/ex2/Program.cs
+++ SixsthHomeWork/ex2/Program.cs
@@ -2,10 +2,16 @@
 // y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 double[] Interceprion(double[] col){
-    double x = (col[2]-col[0])/(col[1]-col[3]);
-    double y = (col[1]*x) + col[0];
-    double[] arr = {x, y};
-    return arr;
+    if(col[1] == col[3]){
+        System.Console.WriteLine("Прямые параллельны, точки пересечения нет");
+        return null;
+    }
+    else{
+        double x = (col[2]-col[0])/(col[1]-col[3]);
+        double y = (col[1]*x) + col[0];
+        double[] arr = {x, y};
+         return arr;
+    }
 }
 
 void main(){
